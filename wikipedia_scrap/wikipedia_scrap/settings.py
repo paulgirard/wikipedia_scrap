56dbd90c967e98aa84ae6620810e9034a656cfb5
ekborra <erik@erikborra.net>
2011-07-20 15:06:20 +0200

    added configuration settings

--- wikipedia_scrap/wikipedia_scrap/settings.py
+++ wikipedia_scrap/wikipedia_scrap/settings.py
@@ -1,18 +1,43 @@
 # Scrapy settings for wikipedia_scrap project
 #
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
+# Here all settings are duplicated which *might* be relevant for the wikipedia_srap project
+# If non-default values are chosen, a comment with the default value is provided
 #
-#     http://doc.scrapy.org/topics/settings.html
-#
+# More info about settings here: http://doc.scrapy.org/topics/settings.html
+# Default values here: https://github.com/insophia/scrapy/blob/master/scrapy/settings/default_settings.py
 
-BOT_NAME = 'wikipedia_scrap'
+BOT_NAME = 'wikipediaInlinks'
 BOT_VERSION = '1.0'
 
-SPIDER_MODULES = ['wikipedia_scrap.spiders']
-NEWSPIDER_MODULE = 'wikipedia_scrap.spiders'
-DEFAULT_ITEM_CLASS = 'wikipedia_scrap.items.WikipediaScrapItem'
+SPIDER_MODULES = ['wikipediaInlinks.spiders']
+NEWSPIDER_MODULE = 'wikipediaInlinks.spiders'
+DEFAULT_ITEM_CLASS = 'wikipediaInlinks.items.WikipediainlinksItem'
 USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
-DEPTH_LIMIT=0
+LOG_ENABLED = True                  
+LOG_LEVEL = 'DEBUG'                 # options: CRITICAL, ERROR, WARNING, INFO, DEBUG
+LOG_FILE = 'scrape.log'             # default = None
 
+CONCURRENT_ITEMS = 100
+CONCURRENT_REQUESTS_PER_SPIDER = 8
+CONCURRENT_SPIDERS = 8
+DEFAULT_RESPONSE_ENCODING = 'utf-8' # default = ascii
+
+DEPTH_LIMIT = 0
+DEPTH_STATS = True
+
+DOWNLOADER_STATS = True
+DOWNLOAD_TIMEOUT = 180
+DOWNLOAD_DELAY = 0
+RANDOMIZE_DOWNLOAD_DELAY = True
+
+MEMUSAGE_NOTIFY_MAIL = False
+MEMUSAGE_REPORT = False
+MEMUSAGE_WARNING_MB = 0
+
+REDIRECT_MAX_TIMES = 20
+
+SCHEDULER_ORDER = 'DFO'             # DFO or BFO
+
+STATS_DUMP = True                   # default = False
+STATS_ENABLED = True
