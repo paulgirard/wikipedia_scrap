0466063e9bab585f510c6af52f2eb97c3244f57a
Paul Girard <paulgirard@uBuntu.(none)>
2011-07-21 02:17:05 +0200

    made NULL possible for parentid in dbschema; made sure the spider always has instantiation of each revision object field

--- wikipedia_scrap/wikipedia_scrap/spiders/wikipedia_spider.py
+++ wikipedia_scrap/wikipedia_scrap/spiders/wikipedia_spider.py
@@ -25,9 +25,10 @@ class Wikipedia_scrapSpider(CrawlSpider):
     "http://pt.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=Aquecimento_global",
     "http://id.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=Pemanasan_global",
     "http://it.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=Riscaldamento_globale",
-    "http://fr.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=R%C3%A9chauffement_climatique",]
+    "http://fr.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=R%C3%A9chauffement_climatique",
+    "http://ru.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=%D0%93%D0%BB%D0%BE%D0%B1%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D0%BF%D0%BE%D1%82%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5",
+    ]
     
-    #"http://ru.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=ids|timestamp|flags|user|size|userid|tags|comment|content&rvlimit=200&titles=%D0%93%D0%BB%D0%BE%D0%B1%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D0%BF%D0%BE%D1%82%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5",
     
     
     
@@ -73,6 +74,8 @@ class Wikipedia_scrapSpider(CrawlSpider):
                     for fieldname in ["revid","parentid","size","timestamp","comment","userid"] :
                         if fieldname in revision.keys() :
                             rv[fieldname]=revision[fieldname]
+                        else :
+                            rv[fieldname]=NULL
                     rv["content"]=revision["*"]
                     rv["tags"]=",".join(revision["tags"])
                     rv["minor"]="minor" in revision.keys()
